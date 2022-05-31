3019c5a6b6d76984f6021a559c36ea103eda545e
Dharmikpaneliy <dharmikpaneliya7@gmail.com>
2022-05-31 08:47:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Medicine from './Container/Medicine/Medicine';
+import Doctor from './Container/Doctor/Doctor';
+import Layout from './Components/Layout/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <Switch>
+          <Route exact path={"/medicine"} component={Medicine} />
+          <Route exact path={"/doctor"} component={Doctor} />
+        </Switch>
+      </Layout>
+    </>
   );
 }
 
